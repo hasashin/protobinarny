2e064345205ac0b6182e3dfdfe40bdb2d4856ccc
paula38 <paulak7245@gmail.com>
2018-11-12 16:16:14 +0100

    <3

--- src/Klient.java
+++ src/Klient.java
@@ -2,37 +2,39 @@ import java.net.*;
 import java.util.Scanner;
 import java.io.*;
 
-public class Klient {
+public class Klient implements Runnable{
 
     int id;
     int odp;
     Socket clientsocket;
+    DataInputStream in;
+    DataOutputStream out;
 
     Klient(ServerSocket ssocket, int clientid){
         try{
             clientsocket = ssocket.accept();   // łączenie socketów ze sobą
+            in = new DataInputStream(clientsocket.getInputStream());
+            out = new DataOutputStream(clientsocket.getOutputStream());
             id = clientid;
         }
-        catch(java.io.IOException e){}
-    }
-
-    public void uzyskajid() {
-        //odbierz od serwera
+             catch(java.io.IOException e){}
     }
 
     public void przeslijodp(){
         Scanner odczyt = new Scanner(System.in);
         odp = Integer.parseInt(odczyt.nextLine());
-        //przeslij do serwera
-    }
-
-    public void polacz(){
-        // uzyskaj server socket
+        try {out.write(new Packet((byte)0, (byte)odp, (byte)id).getPacket());  }
+              catch(java.io.IOException e){}
     }
 
     public void zakoncz(){
         try{ clientsocket.close(); }
-        catch(java.io.IOException e){}
+             catch(java.io.IOException e){}
+    }
+
+    public void run(){
+
+
     }
 
 }
--- src/Serwer.java
+++ src/Serwer.java
@@ -1,5 +1,8 @@
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.time.LocalTime;
+import java.time.Period;
+import java.util.Date;
 import java.util.Random;
 import java.util.Vector;
 
@@ -9,6 +12,8 @@ public class Serwer {
     int id1;
     int id2;
     int czasrozgrywki;
+    //LocalTime poczatkowyczas;
+    long poczatkowy;
     int liczba;
     Packet pakiet = new Packet();
     ServerSocket socket;
@@ -31,33 +36,44 @@ public class Serwer {
 
     public void losujliczbe() {
         Random generator = new Random();
-        liczba = (generator.nextInt(1024));
+        liczba = (generator.nextInt(7));
     }
 
     public void sprawdz() {
         if (pakiet.getAnswer() == liczba){
-            System.out.println("Wartosc odgadnieta! Tajna liczba to: " + liczba);
+           // System.out.println("Wartosc odgadnieta! Tajna liczba to: " + liczba);
         }
         if (pakiet.getAnswer() < liczba){
-            System.out.println("Za mało! Probuj dalej");
+           // System.out.println("Za mało! Probuj dalej");
         }
         if (pakiet.getAnswer() > liczba){
-            System.out.println("Za duzo! Probuj dalej");
+           // System.out.println("Za duzo! Probuj dalej");
         }
     }
 
     public void ileczasu(){
-        //System.out.println("Zostalo " + );
+        long obecny = System.currentTimeMillis()/1000;
+        long uplynelo = obecny - poczatkowy;
+        long zostalo = czasrozgrywki - uplynelo;
+        System.out.println("Zostalo " + zostalo + " sekund");
+
+        // LocalTime obecny = LocalTime.now();
+        //long epoch = System.currentTimeMillis()/1000;
+        // aktualny - poczatkowy = ile upłynęło od uruchomienia
+        // czas max - ile uplynelo od uruchomienia = zostało
     }
 
     public void wyslijpakiet(){
-        
+
     }
 
     public void start(){
         generuj();
         Klient k1 = new Klient(socket, id1);
         Klient k2 = new Klient(socket, id2);
+        //poczatkowyczas = LocalTime.now();
+        long poczatkowy = System.currentTimeMillis()/1000;
+        //ileczasu();
         maxczas();
         losujliczbe();
 
